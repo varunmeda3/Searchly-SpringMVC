c2e6aec98b692a8cf08385f72ffab0d9a45ba396
varunmeda3 <medavarun3@gmail.com>
2025-03-27 02:47:52 +0530

    Edit SearchController

--- Searchly/src/main/java/com/searchly/SearchController.java
+++ Searchly/src/main/java/com/searchly/SearchController.java
@@ -1,1 +1,29 @@
+package com.searchly;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+
+    @RequestMapping("/home")
+    public String home (){
+        return "home";
+    }
+
+    @RequestMapping("/search")
+    public RedirectView search(@RequestParam("query") String query){
+        RedirectView redirectView = new RedirectView();
+        String url = "https://www.google.com/search?q=" + query;
+
+        if (query.length() == 0)  
+            redirectView.setUrl("redirect:/home");
+        else  
+            redirectView.setUrl(url);
+        
+        return redirectView;
+    }
+}
 
